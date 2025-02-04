2fd2cfe972362c824f848329b9f066a46ffea927
swciitg <swciitghy@gmail.com>
2025-02-04 18:43:16 +0530

    migrate to dotenv

--- packages/gate_log/lib/src/globals/endpoints.dart
+++ packages/gate_log/lib/src/globals/endpoints.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Endpoints {
-  static final khokhaWebSocketUrl = dotenv.env['GATELOG-WEBSOCKET-URL']!;
-  static final gateLogServerUrl = dotenv.env['GATELOG-SERVER-URL']!;
-  static final onestopSecurityKey = dotenv.env['SECURITY-KEY']!;
-
-  static const onestopBaseUrl = String.fromEnvironment('SERVER-URL');
+  static final khokhaWebSocketUrl = dotenv.env['GATELOG_WEBSOCKET_URL']!;
+  static final gateLogServerUrl = dotenv.env['GATELOG_SERVER_URL']!;
+  static final onestopSecurityKey = dotenv.env['SECURITY_KEY']!;
+  static final onestopBaseUrl = dotenv.env['SERVER_URL']!;
+  
   static const String getAllLogs = '/history';
 
   static getHeader() {
